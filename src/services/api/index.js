686df2edad3ea3c1c7a63c2e2a86f23b615831d2
MarlonAnacona <ejemplo@email.com>
2023-02-01 15:28:58 -0500

    guardar

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,10 +1,11 @@
-const URL = "http://localhost:3000";
+const URL = "http://localhost:8000";
 
 const endpoints = {
-  clients: {
-    getAll: `${URL}/cliente`,
+  users: {
+    getAll: `${URL}/users`,
+    login: `${URL}/users/token`,
+    register: `${URL}/users/register-user`,
   },
-  users: {},
   products: {},
   workers: {},
 };
